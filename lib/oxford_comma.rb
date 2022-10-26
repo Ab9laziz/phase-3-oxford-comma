abb88d7bccc0157f92045d25f679c2f25eb117e1
Abdulaziz Abdullahi <abdulaziz.abdullahi@student.moringaschool.com>
2022-10-26 12:55:20 +0300

    complete

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def oxford_comma(array)
-
+  if array.length==1
+    array.join
+  elsif array.length == 2 
+    array.join(' and ')
+  elsif array.length == 3
+    "#{array[0..1].join(', ')}, and #{array[-1]}"
+  elsif array.length > 3
+    "#{array[0...-1].join(', ')}, and #{array[-1]}"
+  end
 end
+
+oxford_comma ["fiddleheads", "okra", "kohlrabi"]
